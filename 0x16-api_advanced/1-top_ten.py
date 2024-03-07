4e834edc056c096188d9fbbf1c660cce75b1eb5e
Abdessadek EL AASRI <elaasriya2003@gmail.com>
2024-03-07 15:52:02 +0100

    hot

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-import requests
-
 
 """top_ten"""
 
@@ -8,35 +6,20 @@ import requests
 def top_ten(subreddit):
     """get top ten hot titles of a certain subreddit """
 
-    client_id = "PpEfMsSxFbxQHsIMzr5faQ"
-    client_secret = "8yTk0bJrYf78NPzdONjt1zWRmYo8SQ"
-    r_usr = "tarekElaasri"
-    r_pass = "(k4r9CQ^^?wN46L"
-    url = "https://www.reddit.com/api/v1/access_token"
-    auth_data = {"grant_type": "password", "username": r_usr,
-                 "password": r_pass}
-    auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
-    header = {'user-agent':
-              'script:subscriber_counter_v0.1 by /u/tarekElaasri'}
-    res = requests.post(url, data=auth_data,
-                        headers=header,
-                        auth=auth)
+    import requests
 
-    if res.status_code == 200:
-        token = res.json()['access_token']
-    else:
-        print(res)
-
-    url = "https://oauth.reddit.com/r/{}/hot?limit=9".format(subreddit)
-    headers = {'Authorization': 'bearer ' + token,
-               'User-Agent':
-               'script:subscriber_counter_v0.1 by /u/tarekElaasri'}
+    url = "https://oauth.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {'User-Agent':
+               'script:subscriber_counter_v0.1 by /u/tarekElaasri'
+               }
     res = requests.get(url, headers=headers, allow_redirects=False)
+
     if res.status_code == 200:
-        titles = [item['data']['title']
-                  for item in res.json()['data']['children']
-                  if item['kind'] == 't3']
-        for title in titles:
-            print(title)
-    else:
-        return 0
+        if res.status_code == 200:
+            titles = [item['data']['title']
+                      for item in res.json()['data']['children']
+                      if item['kind'] == 't3']
+            for title in titles:
+                print(title)
+        else:
+            return 0
