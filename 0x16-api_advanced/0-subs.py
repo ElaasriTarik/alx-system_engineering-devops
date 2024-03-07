242f172d732a0525ed02856bfeceb1c817eb2554
Abdessadek EL AASRI <elaasriya2003@gmail.com>
2024-03-07 15:43:37 +0100

    hot

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-
-"""number_of_subscribers"""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """get number of subscribers to a certain
-    subreddit """
-
-    import requests
-    
+    """
+    Get the number of subscribers to a certain subreddit.
+    """
     url = "https://oauth.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent':
-               'script:subscriber_counter_v0.1 by /u/tarekElaasri'
-               }
+    headers = {
+        'User-Agent': 'script:subscriber_counter_v0.1 by /u/tarekElaasri'
+    }
     res = requests.get(url, headers=headers, allow_redirects=False)
 
     if res.status_code == 200:
